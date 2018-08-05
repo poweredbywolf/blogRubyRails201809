5be63acc519db56921ce69cbd9b98ab86e5ae768
Ze-ev Krein <zvkrein@gmail.com>
2018-08-05 20:05:43 +0200

    Added portfolio item via Resource gneretator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world."
+  )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "My great service",
+    body: "Unthinking respect for authority is the greatest enemy of truth.",
+    main_image: , "https://place-hold.it/300x200"
+    thumb_image: "https://place-hold.it/350x200"  
+  )
+
+end
+
+put "9 portfolio items created"
