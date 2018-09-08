39e2191a5ec1c9a4bf4857056666427b2ed1dcb9
Ze-ev Krein <zvkrein@gmail.com>
2018-09-08 00:01:59 -0700

    validation for skills blog and portfolio feature

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,9 @@
 class Blog < ApplicationRecord
+    # enum status: {draft: 0, published: 1}
+    # extend FriendlyId
+    # friendly_id :title, use: :slugged
+
+    validates_presence_of :title, :body
+# validates_presence_of ensures that a blog that is created must have a title and body
+# PRESENCE OF
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,6 @@
 class Portfolio < ApplicationRecord
+
+     validates_presence_of :title, :body, :main_image, :thumb_image
+# validates_presence_of ensures that a blog that is created must have a title and body
+# PRESENCE OF
 end
